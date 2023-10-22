a8bcd5941eaa23e7fdf73f794d344ec669bed10f
AbstractProgrammer <AbstractProgrammer@example.com>
2023-10-22 20:31:46 +0300

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
   return (
     <header id="home">
       <div className="container header__container">
-        <h5>Hello I am</h5>
+        <h3>Hello I'm</h3>
         <h1>Proff MohaMett TahLiiL</h1>
-        <h5 className="text-light">Full-stack Developer</h5>
+        <h5 className="text-light">Full-stack Web Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Rebeat Again 
